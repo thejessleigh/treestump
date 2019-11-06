f4fa3a14a16ce0d17bc4f83d8c80e940a28bc0ac
thejessleigh <j.l.unrein@gmail.com>
2019-11-05 18:54:58 -0600

    add logger and formatter

---
+++ treestump/formatter.py
@@ -0,0 +1,62 @@
+import json
+import logging
+import socket
+
+
+class TreestumpFormatter(logging.Formatter):
+    def __init__(self, application: str, *args, **kwargs):
+        """
+        @param application: the name of the application using the logger
+        @type application: str
+        """
+        super().__init__(*args, **kwargs)
+        self.application = application
+        self.host = socket.gethostname()
+
+    def _json_format(self, log_record: logging.LogRecord) -> dict:
+        """
+        Defines the dictionary ready to be dumped as JSON.
+
+        This method collects basic useful information from the Python logging.LogRecord object. It is designed to be extended by subclasses calling `super()` to extend the dictionary with application specific logging information. This allows developers to easily modify the log message *before* it is dumped as JSON, and prevents the user from needing to override the `format` method itself.
+
+        @note This method should only be called by `TreestumpFormatter.format()`
+        @param record: The log record that needs to be formatted as JSON
+        @type record: logging.LogRecord
+        @return: A dictionary containing specific record items, ready to be dumped as JSON
+        @rtype: dict
+        """
+        exception = log_record.exc_info
+        record_dict = {
+            "timestamp": log_record.created,
+            "humanTime": log_record.asctime,
+            "message": log_record.getMessage(),
+            "host": self.host,
+            "application": self.application,
+            "type": "exception" if exception else "log",
+            "level": log_record.levelname,
+            "levelNumber": log_record.levelno,
+            "logger": log_record.name,
+            "path": log_record.pathname,
+            "filename": log_record.filename,
+            "module": log_record.module,
+            "function": log_record.funcName,
+            "line": log_record.lineno,
+            "processId": log_record.proces,
+            "processName": log_record.processName,
+            "threadName": log_record.threadName,
+            "stackInfo": "".join(
+                self.FormatStack(log_record.stack_info)
+            ),  # this may be too verbose - need feedback from folks
+        }
+        if exception:
+            record_dict.update({"trace": "".join(self.FormatException(exception))})
+        return record_dict
+
+    def format(self, log_record: logging.LogRecord) -> str:
+        """
+        @param log_record: the incoming Python logging.LogRecord object
+        @type log_record: logging.LogRecord
+        @return: JSON formatted string containing log record information
+        @rtype: str
+        """
+        return json.dumps(log_record, ensure_ascii=False)
---
+++ treestump/logger.py
@@ -0,0 +1,59 @@
+import typing
+
+from formatter import TreestumpFormatter
+
+
+class TreestumpLogger:
+    def __init__(self, app_name: str, event_log_formatter_cls=None):
+        """
+        Creates the TreestumpLogger instance
+
+        @param app_name: The name of the application where the logger is being used
+        @type app_name: str
+        @debug_mode: (Optional) Defaults to False
+        @type debug_mode: bool
+        @param event_log_formatter_cls: (Optional) Log formatter for the class. Defaults to `TreestumpFormatter`. Any custom formatters should inherit from `TreestumpFormatter`.
+        """
+        self.app_name = app_name
+        if event_log_formatter_cls:
+            self._event_log_formatter_cls = event_log_formatter_class
+        else:
+            self.event_log_formatter_cls = TreestumpFormatter
+
+        self._logger = self._configure_logger()
+
+    def __repr__(self):
+        return f"TreestumpLogger for application {self.app_name}"
+
+    def log(self, message: str, *args, **kwargs):
+        """
+        @param message: The human-readable message to log
+        @type message: str
+        @param args: Additional arguments to pass to the logger
+        @type args: list
+        @param kwargs: Additional keyword arguments to pass to the logger
+        @type kwargs: dict
+        """
+        # If no level is specified, log at 20: INFO
+        level = kwargs.pop("level", 20)
+        self._logger.log(level, *args, **kwargs)
+
+    def _configure_logger(self):
+        """
+        Configures the Python logger. This retreives or creates a specially-named logger where the handler is an instance of `logging.StreamHandler` and the formatter is an instance of `TreestumpFormatter`
+
+        @return: The configured Python logger
+        @rtype: logging.Logger
+        """
+        # Create (or retrieve) a logger named by our template and app_name
+        logger = logging.getLogger(f"{self.app_name}_event_logger")
+
+        # Set log level threshold
+        logger.setLevel(logging.INFO)
+
+        # Create a new logging.StreamHandler() and set our defined formatter on the handler
+        handler = logging.StreamHandler()
+        handler.setFormatter(self._event_log_formatter_cls(self.app_name))
+        logger.addHandler(handler)
+
+        return logger
